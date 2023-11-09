174764403177a744e30f5cd7f726df2b7d81a03b
Julien Décharne <jdecharne@youwol.com>
2023-11-09 09:19:42 +0100

    ⬆️ `@youwol/vsf-core` 0.2.3 =» 0.2.4

--- template.py
+++ template.py
@@ -28,7 +28,7 @@ template = Template(
     dependencies=Dependencies(
         runTime=RunTimeDeps(
             externals={
-                "@youwol/vsf-core": "^0.2.3",
+                "@youwol/vsf-core": "^0.2.4",
                 "@youwol/cdn-client": "^2.0.6",
                 "three": "^0.152.0",
                 "rxjs": "^6.5.5",
